42642bd9d8e9851521836ad267d4e60f9d52a10f
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2021-12-07 00:58:18 +0000

    add validation to string and int inputs, add clear terminal func

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import os
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -13,53 +14,51 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('call_data')
 
 
-
+#string input only
 def get_rep_name():
     """
     Get the representative's name from the user.
     Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string. The loop will
+    via the terminal, which must contain only alpha numeric values. The loop will
     repeatedly request data until the data is valid.
     """
+    clear_terminal()
+    num = None
+    print("We need the representative's name.\n")
 
-    while True:
-        print("We need the representative's name.\n")
-
-        rep_name_input = input("Please enter you name here: ")
-
-        test = rep_name_input
-        print(test)
-
-        if validate_int(rep_name_input):
-            all_input_data.append(rep_name_input)
-            print(all_input_data)
+    while num is None:
+        input_value = input("Please enter your name here: ").lower().strip()
+        if input_value.isalpha():
+            num = input_value
             
-        
-            get_dept_name() 
-            break
-            
+            print("Thank you for your name \n")
+            get_dept_name()
+        else:
+            print("Sorry you have entered {input}, please enter text only.".format(input=input_value))
 
+
+#string input only
 def get_dept_name():
     """
-    Description needed
+    Get the department name from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must contain only alpha numeric values. The loop will
+    repeatedly request data until the data is valid.
     """
-    
+    clear_terminal()
+    print("Now we need your department name. \n")
     while True:
-        print("Now we need your department name.")
+        dep_name_input = input("Please enter the name of your department: ").lower().strip()
 
-        dep_name_input = input("Please enter the name of your department: ")
+        if dep_name_input.isalpha():
+            print("Thank you for your department name \n")
 
-        if validate_int(dep_name_input):
-            all_input_data.append(dep_name_input)
-            print(all_input_data)
-            
-        
-            get_inbound_calls()
-            break
+        else: 
+            print("Sorry you have entered {input}, please enter text only.".format(input=dep_name_input))  
 
 
 
-
+#num
 def get_inbound_calls():
     """
     Get call figures input from the user.
@@ -68,60 +67,26 @@ def get_inbound_calls():
     will repeatedly request data until the data is valid.
     """
 
-    while True:   
+    while True: 
         print("Enter the number of inbound calls received.\n")
 
-        inbound_input = input("Enter your data here: ")
+        try:
 
-        inbound_calls = inbound_input
-        print(inbound_calls)
-       
+            inbound_input = int(input("Enter your data here: "))
 
-        if validate_int(inbound_calls):
-            print("Data is valid!")
-            break
-
-    return inbound_calls
-
+        except ValueError:
+            print(f"You have entered characters, please ensure it is only numbers")
+            continue
+        else:
+            print("that all worked ok")
+            break   
 
 
+#num
 def get_dropped_calls():
     """
     Description needed
-    """
-
-
-
-
-
-def validate_string(values):
-    """
-    Description needed
-    """
-
-
-
-
-
-def validate_int(values):
-    """
-    Converts string values into integers.
-    Raises ValueError if strings cannot be coverted into int,
-    or if there is more than one value.
-    """
-    try:
-        [int(value) for value in values]
-        if len(values) != 1:
-            raise ValueError(
-                f"Only 1 value is required, you provided {len(values)}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
-
-
-    return True
-
+    """ 
 
 # def update_call_worksheet(data):
 #     """
@@ -135,6 +100,10 @@ def validate_int(values):
 
 
 # data = all_input_data
+
+def clear_terminal():
+    os.system('clear')
+
 
 all_input_data = []
 get_rep_name()
