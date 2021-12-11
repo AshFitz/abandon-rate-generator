87445605d6e633bd360694cdbac5c5d9510d22d0
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2021-12-11 03:14:38 +0000

    Add validate text and number func

--- generator/validations.py
+++ generator/validations.py
@@ -1,8 +1,6 @@
-if all(x.isalpha() or x.isspace() for x in input_value):
-                num = input_value
-                all_input_data.append(input_value)
-                
-                print("Thank you!.\n")
-                get_job_title()
-            else:
-                print("Sor
+def validate_text(input_value):
+    return False
+
+
+def validate_numbers(input_value):
+    return False
