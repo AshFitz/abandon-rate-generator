8262ff34e2559716f744e28f9a74000568b8614e
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2021-12-12 10:44:26 +0000

    add doc string to run.py file

--- run.py
+++ run.py
@@ -1,17 +1,21 @@
 from generator.call_generator import Generator
 
+"""
+Main class that is used to initialize
+a new instance of the Generator class,
+the run method is called, when called
+it finds the current instance, calls
+the function for start_generator.
+"""
+
 
 class Main:
-    """
-    Constructor for class main.
-    """
 
     def __init__(self):
         self.page = Generator()
 
     def run(self):
         self.page.start_generator()
-        # self.page.get_dropped_calls()
 
 
 main = Main()
